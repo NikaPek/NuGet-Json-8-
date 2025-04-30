c1ffc11d47bb4425e13f0309aca868ed346784b2
NikaPek <veronika.pekarskaite@sa.stud.vu.lt>
2025-04-30 20:47:08 +0300

    1,2,3 punktai

---
+++ JsonExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+class Program
+{
+   
+    //aprasyti klase atitinkancia json struktura
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        //nuoroda i json fila
+        string filePath = $"{Environment.CurrentDirectory}\\user.json";
+        
+        //perskaito json failo duomenis
+        string jsonResponse = File.ReadAllText(filePath);
+        
+        //json failas deserializuojamas i C# objekta
+        User user = JsonConvert.DeserializeObject<User>(jsonResponse);
+        
+        //atspauzdinama vartotojo(user) informacija
+        Console.WriteLine($"Name: {user.Name}");
+        Console.WriteLine($"Age: {user.Age}");
+        Console.WriteLine($"City: {user.City}");
+    }
+}
---
+++ JsonExample/Readme.md
@@ -0,0 +1,2 @@
+1. Sukuriu json faila ir xml failus, juos užpildau duomenimis iš pavyzdžio, taip pat nukopijavau pateikta pavizdyje koda – json failo deserializavimui i c# objekta
+2. 
